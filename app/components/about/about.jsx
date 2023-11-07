409a77cf6558620496a92c8ec125ebacdc78a516
Filip-Groh <filip.groh.023@pslib.cz>
2023-11-07 18:13:28 +0100

    About page

---
+++ app/components/about/about.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+const About = ({isRight, heading, text, image}) => {
+    return (
+        <div className="hero min-h-[20rem] bg-base-200">
+            <div className={`hero-content flex-col ${isRight ? "lg:flex-row-reverse" : "lg:flex-row"}`}>
+                <Image className="max-w-sm rounded-lg shadow-2xl hover:scale-[110%] sm:hover:scale-[150%] transition-transform" width={384} height={0} src={image} alt="Spiderman" />
+                <div>
+                    <h1 className="text-5xl font-bold">{heading}</h1>
+                    <p className="py-6">{text}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- app/o-nas/page.jsx
+++ app/o-nas/page.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import NavigationPath from '../components/navigation/navigation'
+import About from '../components/about/about'
+import Welcome from '../components/about/welcome'
 
 const AboutPage = () => {
     return (
         <>
             <NavigationPath path={["Home", "O nás"]}/>
-            <h1>INTERIERY  DESIGN  s.r.o. – designové  studio v Liberci</h1>
-            <h2>Konzultace, poradenství, studie, návrhy a konečná realizace. Tradice 19 ti let kvalitních služeb – je vytvoření designového interiéru se specifickou atmosférou v souladu s představami a nároky klienta</h2>
-            <p>Cílem interiérového studia v Liberci je, aby spolupráce s bytovým designérem byla dostupnou možností pro každého. Soukromé i komerční interiéry po úvodní konzultaci navrhneme, barevně sladíme a kompletně vybavíme nábytkem vyrobeným na míru dle Vašich potřeb, bytovým textilem (záclony a závěsy) a bytovými doplňky. Ušití bytového textilu na zakázku dle požadavků zákazníka i pro komerční klientelu – ušití napronů, ubrusů a rautových sukní. Čalounické práce – výběr potahových tkanin a vlastní provedení čalounění. Zajistíme služby související s realizací. Přestavba, montáž a instalace – veškeré odborné práce související se změnou interiéru a zařízení. Odborné zpracování technické dokumentace a vlastní provedení –  elektrikářské a instalatérské práce, sádrokartonové, malířské a tapetářské práce, podlahářské a pokladačské práce. Technické zajištění organizace a dozor provedených vlastních prací. Provádíme HOME STAGING. Pro snadnější výběr máme katalogy vzorníků materiálů pro výrobu nábytku na zakázku, podlahových krytin, tapet, osvětlení, potahových tkanin, záclon, závěsů, stínění oken a bytových doplňků. Výběr může probíhat přímo ve Vašem interiéru. </p>
-            <h2>Náplň interiérového designéra</h2>
-            <p>Vytvořit příjemné a vkusné interiéry domova a sladit v dokonalý celek. Interiérový designér vytváří vnitřní strukturu interiéru (vč. technických věcí), jeho vybavení až po bytové doplňky. Tím dodá interiéru osobitou a jedinečnou atmosféru s respektováním životního stylu klienta. Designový návrh – obnáší celkové řešení a zcela nový ucelený projekt daných  interiérů Redesign interiéru – návrh respektuje stávající zařízení a osobitost interiéru a k proměně dochází drobnými úpravami, částečnou obměnou zařízení nebo barevným sladěním.</p>
-            <h2>Co je HOME STAGING ?</h2>
-            <p>Home staging je profesionální příprava nemovitosti k prodeji nebo pronájmu. Je to soubor znalostí a pravidel, které dokáží dát domu nebo bytu osobitou atmosféru, která zaujme potencionální kupce. Jde o sladění barevných kombinací, účelné rozmístění nábytku, doplňků a osvětlení. Tato činnost úzce souvisí s činností interiérového designéra. Naše studio tuto službu nabízí nejenom soukromím osobám – majitelům nemovitosti, ale i realitním kancelářím, makléřům a developerům.</p>
+            <Welcome />
+            <About isRight={false} heading="Co je cílem?" text="Interiérové studio v Liberci nabízí návrh, sladění a vybavení bytů a komerčních prostor. Vytvoříme nábytek na míru, textil a doplňky. Ušijeme textil na zakázku, včetně čalounických prací. Zajišťujeme přestavby, montáže, instalace a HOME STAGING. Nabízíme katalogy vzorníků materiálů." image="/spiderman.jpg"/>
+            <About isRight={true} heading="Náplň interiérového designéra" text="Vytvořit příjemné a vkusné interiéry domova a sladit v dokonalý celek. Interiérový designér vytváří vnitřní strukturu interiéru (vč. technických věcí), jeho vybavení až po bytové doplňky. Tím dodá interiéru osobitou a jedinečnou atmosféru s respektováním životního stylu klienta." image="/spiderman.jpg"/>
+            <About isRight={false} heading="Co je HOME STAGING?" text="Home staging je profesionální příprava nemovitosti k prodeji nebo pronájmu. Zahrnuje barevnou koordinaci, nábytek a osvětlení. Nabízíme tuto službu soukromým osobám i realitním kancelářím, makléřům a developerům." image="/spiderman.jpg"/>
         </>
     )
 }
