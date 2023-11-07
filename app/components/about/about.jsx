cd23ea3168e95acb80e5262cf9625bce7698acff
Filip-Groh <filip.groh.023@pslib.cz>
2023-11-07 18:18:35 +0100

    Bug fix

--- app/components/about/about.jsx
+++ app/components/about/about.jsx
@@ -5,7 +5,7 @@ const About = ({isRight, heading, text, image}) => {
     return (
         <div className="hero min-h-[20rem] bg-base-200">
             <div className={`hero-content flex-col ${isRight ? "lg:flex-row-reverse" : "lg:flex-row"}`}>
-                <Image className="max-w-sm rounded-lg shadow-2xl hover:scale-[110%] sm:hover:scale-[150%] transition-transform" width={384} height={0} src={image} alt="Spiderman" />
+                <Image className="max-w-sm rounded-lg shadow-2xl hover:scale-[110%] sm:hover:scale-[150%] transition-transform" width={450} height={0} src={image} alt="Spiderman" />
                 <div>
                     <h1 className="text-5xl font-bold">{heading}</h1>
                     <p className="py-6">{text}</p>
