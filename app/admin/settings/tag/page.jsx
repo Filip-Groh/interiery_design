5b7ee0b69a2b875bce4503fd0a04786acef820a7
Filip-Groh <filip.groh.023@pslib.cz>
2024-02-22 18:47:32 +0100

    Tag Fix

--- app/admin/settings/tag/page.jsx
+++ app/admin/settings/tag/page.jsx
@@ -1,12 +1,12 @@
 import TagEditable from '@/app/components/settings/tag/tag-settings'
 import React from 'react'
-import { getComment } from '@/utils/database'
+import { getTag } from '@/utils/database'
 import SettingsMenu from '@/app/components/settings/menu'
 import AddTag from '@/app/components/settings/tag/add'
 import TagDialog from '@/app/components/settings/tag/dialog'
 
 const TagSettings = async () => {
-    const tags = await getComment()
+    const tags = await getTag()
 
     return (
         <SettingsMenu activeTabName="Tag">
--- app/components/settings/tag/tag-settings.jsx
+++ app/components/settings/tag/tag-settings.jsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React from 'react'
-import Image from 'next/image'
 import DeleteButton from '../deleteButton'
 
 const TagEditable = ({id, name, isDependent}) => {
